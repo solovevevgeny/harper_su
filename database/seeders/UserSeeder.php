976d9feed9a40b69fc3b8386158f0b51f9160dd3
Solovev Evgeny <evgeny.solovev@gmail.com>
2024-03-22 11:45:08 +0300

    model, migration, seeder (USER))

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = new User();
+        $user->name = "Евгений";
+        $user->email = "evgeny.solovev@gmail.com";
+        $user->password = "asd";
+        $user->save();
+
+    }
+}
