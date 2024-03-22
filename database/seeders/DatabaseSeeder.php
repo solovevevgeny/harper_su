4a5e9bee03379cf589a27081284a77bb1316e3b6
Solovev Evgeny <evgeny.solovev@gmail.com>
2024-03-22 11:45:24 +0300

    all seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +13,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(UserSeeder::class);
+        $this->call(ContactSeeder::class);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(CompanySeeder::class);
+        $this->call(WarehouseSeeder::class);
+        $this->call(PriceTypeSeeder::class);
+        $this->call(ContactTypeSeeder::class);
+
+        $this->call(ItemSeeder::class);
+        $this->call(OfferSeeder::class);
+        $this->call(PriceSeeder::class);
+
     }
 }
